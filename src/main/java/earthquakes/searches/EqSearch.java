c3cd26dc8f857b29bd3b1e1dcd5860cf07e8ca70
贺华柯 <hehuake@hehuakedeMacBook-Pro.local>
2019-11-29 22:55:50 -0800

    hh - added attribbute to search page

--- src/main/java/earthquakes/searches/EqSearch.java
+++ src/main/java/earthquakes/searches/EqSearch.java
@@ -4,6 +4,9 @@ public class EqSearch{
 
 	private int distance;
 	private int minmag;
+	private double lat;
+	private double lon;
+	private String location;
 
 	public EqSearch(){};
 
@@ -15,11 +18,35 @@ public class EqSearch{
 		return this.minmag;
 	}
 
+	public double getLat(){
+		return this.lat;
+	}
+
+	public double getLon(){
+		return this.lon;
+	}
+
+	public String getLocation(){
+		return this.location;
+	}
+
 	public void setDistance(int distance){
 		this.distance = distance;
 	}
 
 	public void setMinmag(int minmag){
 		this.minmag = minmag;
+	}
+
+	public void setLat(double lat){
+		this.lat = lat;
+	}
+
+	public void setLon(double lon){
+		this.lon = lon;
+	}
+
+	public void setLocation(String location){
+		this.location = location;
 	}
 }
